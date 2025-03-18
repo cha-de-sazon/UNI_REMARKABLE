270f989c3f4c04d19714d09472995094a200f848
cha-de-sazon <104666188+cha-de-sazon@users.noreply.github.com>
2025-03-18 01:24:34 -0300

    Update ep1_09.py

--- ep1_09.py
+++ ep1_09.py
@@ -3,14 +3,13 @@ b = int(100)
 c = int(25)
 d = int(1)
 
-carga = int(input(":"))
+carga = int(input("Carga: "))
 pesos = [a, b, c, d]
+caixas = []
 
-caixas1 = [carga // peso for peso in pesos]
-
-caixas = [caixas1[0], caixas1[1] - ((a // b) * caixas1[0]), caixas1[2] - ((b // c) * caixas1[1]), caixas1[3] - ((c // d) * caixas1[2])]
-
-for i in caixas1:
-    caixas = [caixas1[i] - ((pesos[n] // pesos[n+1]) * caixas1[i-1]) for n in pesos]
+for peso in pesos:
+    peso2 = int(pesos.index(peso) - 1)
+    caixa = (carga // peso) - ((pesos[peso2] // peso) * (carga // pesos[peso2]))
+    caixas.append(caixa)
 
 print(caixas)
